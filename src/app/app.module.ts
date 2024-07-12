1fcca9dab4317c1bb9811a8ebffc99dee7616d87
beattris <beatriceebirim@gmail.com>
2024-07-12 12:19:10 +0100

    set up the project with firebase firestore using Angular Fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC7JwRpCovbKctqHO4lBEZWoGw00jTZhYE',
+    authDomain: 'ng-blog-1a2f6.firebaseapp.com',
+    projectId: 'ng-blog-1a2f6',
+    storageBucket: 'ng-blog-1a2f6.appspot.com',
+    messagingSenderId: '960623986705',
+    appId: '1:960623986705:web:b258d97207d82af65989b0',
+  },
+};
